b94b67da48793a89f949d00e8ce953159fc29c81
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-20 16:10:03 -0400

    Drafted reflect function

--- computer-graphics-raster-images/src/reflect.cpp
+++ computer-graphics-raster-images/src/reflect.cpp
@@ -9,6 +9,25 @@ void reflect(
 {
   reflected.resize(width*height*num_channels);
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  /* Reference code used :
+  *		- https://pencilprogrammer.com/cpp-programs/reverse-array/
+  */
+  int row_count = 0;
+  // iterate through each row separately
+  while( row_count < height ) {
+	  // define the row's start and end pixels (reading from left to right)
+	  int start = 0; 
+	  int end = width - 1; 
+	  // 
+	  while (start <= end) {
+		  for (int rgb = 0; rgb < num_channels; rgb++) { //keep channel values in the right order e.g. {255 0 0  0 255 50  20 30 100} becomes {20 30 100  0 255 50  255 0 0}	
+			  reflected[(start + width*row_count)*num_channels + rgb] = input[(end + width*row_count)*num_channels + rgb];
+			  reflected[(end + width*row_count)*num_channels + rgb] = input[(start + width*row_count)*num_channels + rgb];
+		  }
+		  start++;
+		  end--;
+	  }
+	  row_count++;
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
