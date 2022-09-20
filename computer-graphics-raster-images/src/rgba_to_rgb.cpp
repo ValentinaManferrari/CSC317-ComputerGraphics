ddbcabd1aaf91edbb77f4de412cdab721db1ff10
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-20 17:38:27 -0400

    Drafted rotate function

--- computer-graphics-raster-images/src/rgba_to_rgb.cpp
+++ computer-graphics-raster-images/src/rgba_to_rgb.cpp
@@ -1,22 +1,22 @@
 #include "rgba_to_rgb.h"
 
 void rgba_to_rgb(
-const std::vector<unsigned char> & rgba,
-const int & width,
-const int & height,
-std::vector<unsigned char> & rgb)
+  const std::vector<unsigned char> & rgba,
+  const int & width,
+  const int & height,
+  std::vector<unsigned char> & rgb)
 {
-rgb.resize(height*width*3);
-////////////////////////////////////////////////////////////////////////////
-int rgb_idx = 0;
-for (int rgba_idx = 0; rgba_idx < rgba.size(); ++rgba_idx) {
-	if ((rgba_idx + 1) % 4) {
-		continue; // skip alpha channel 
-	}
-	else {
-		rgb[rgb_idx] = rgba[rgba_idx]; //add RGB channels
-		++ rgb_idx;
-	}
-}
-////////////////////////////////////////////////////////////////////////////
+  rgb.resize(height*width*3);
+  ////////////////////////////////////////////////////////////////////////////
+  int rgb_idx = 0;
+  for (int rgba_idx = 0; rgba_idx < rgba.size(); ++rgba_idx) {
+	  if ((rgba_idx + 1) % 4) {
+		  continue; // skip alpha channel 
+	  }
+	  else {
+		  rgb[rgb_idx] = rgba[rgba_idx]; //add RGB channels
+		  ++ rgb_idx;
+	  }
+  }
+  ////////////////////////////////////////////////////////////////////////////
 }
--- computer-graphics-raster-images/src/rotate.cpp
+++ computer-graphics-raster-images/src/rotate.cpp
@@ -9,6 +9,20 @@ void rotate(
 {
   rotated.resize(height*width*num_channels);
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  int row = 0;
+  while(row < height) {
+      int rotated_col = row;  
+      int start = width - 1;
+      int end = 0;
+      while (start > end) {
+          int rotated_row = width - start - 1;
+          for (int rgb = 0; rgb < num_channels; rgb++) {
+              rotated[(rotated_col + height*rotated_row)*num_channels + rgb] = input[num_channels * (start + width * row) + rgb];
+          }
+          start--;
+          end++;
+      };
+      row++;
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
