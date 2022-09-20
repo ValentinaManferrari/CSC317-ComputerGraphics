6a1f1db87756d07d206f9f59fd4974db8ec69dd0
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-20 10:44:06 -0400

    rgba_to_rgb draft completed (currently not working - reading vector out of bonds)

--- computer-graphics-raster-images/src/rgba_to_rgb.cpp
+++ computer-graphics-raster-images/src/rgba_to_rgb.cpp
@@ -8,6 +8,15 @@ void rgba_to_rgb(
 {
   rgb.resize(height*width*3);
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  int rgb_idx = 0;
+  for (int rgba_idx = 0; rgba_idx = rgba.size(); ++rgba_idx) {
+	  if ((rgba_idx + 1) % 4) {
+		  continue;
+	  }
+	  else {
+		  rgb[rgb_idx] = rgba[rgba_idx];
+		  ++ rgb_idx;
+	  }
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
