85c667ea82d8e6fae5e3a1ca64a5dfc59687e958
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-21 09:42:05 -0400

    implemented working bayer filter function

--- computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
+++ computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
@@ -8,6 +8,38 @@ void simulate_bayer_mosaic(
 {
   bayer.resize(width*height);
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  /* Reference Sources used to write this function:
+        - https://en.wikipedia.org/wiki/Bayer_filter
+        - "In this assignment, we'll assume the top left pixel is green, its right neighbor is blue 
+            and neighbor below is red, and its kitty-corner neighbor is also green."
+  */
+
+  for (int row = 0; row < height; row++) {
+      for (int col = 0; col < width; col++) {
+          int idx = col + width * row;
+          int channel;
+          // odd rows have pattern green-blue-green-blue...
+          if ((row+1) % 2 != 0) {
+              // odd column -> green pixel
+              if ((col+1) % 2 != 0) {
+                  channel = 1;
+              }
+              else { // even column -> blue pixel
+                  channel = 2;
+              }
+          }
+          // it means it's an even row therefore pattern red-green-red-green...
+          else {
+              // odd column -> red pixel
+              if ((col + 1) % 2 != 0) {
+                  channel = 0;
+              }
+              else { // even column -> green pixel
+                  channel = 1;
+              }
+          }
+          bayer[idx] = rgb[3 * idx + channel];
+      }
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
