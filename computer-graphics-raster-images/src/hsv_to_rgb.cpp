15f5c0c7b0f2f30a3b96bc596a362085e5e8cf6b
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-21 11:49:18 -0400

    completed rgb<->hsv conversion functions

--- computer-graphics-raster-images/src/hsv_to_rgb.cpp
+++ computer-graphics-raster-images/src/hsv_to_rgb.cpp
@@ -1,4 +1,6 @@
 #include "hsv_to_rgb.h"
+#include <cmath>
+#include <algorithm>
 
 void hsv_to_rgb(
   const double h,
@@ -9,9 +11,41 @@ void hsv_to_rgb(
   double & b)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  r = 0;
-  g = 0;
-  b = 0;
+  /* Reference Sources used to write this function:
+        - https://www.had2know.org/technology/hsv-rgb-conversion-formula-calculator.html#:~:text=Converting%20HSV%20to%20RGB&text=For%20example%2C%20if%20H%20%3D%20135,you%20divide%20it%20by%202.&text=B%20%3D%20m.&text=B%20%3D%20m.,-If%20120%20%E2%89%A4
+  */
+    double M = 255 * v;
+    double m = M * (1 - s);
+    double z = (M - m)*(1 - std::fabs(std::fmod((h / 60), 2) - 1));
+    if (h >= 0 && h < 60) {
+        r = M;
+        g = z + m;
+        b = m;
+    }
+    else if (h >= 60 && h < 120) {
+        r = z + m;
+        g = M;
+        b = m;
+    }
+    else if (h >= 120 && h < 180) {
+        r = m;
+        g = M;
+        b = z + m;
+    }
+    else if (h >= 180 && h < 240) {
+        r = m;
+        g = z + m;
+        b = M;
+    }
+    else if (h >= 240 && h < 300) {
+        r = z + m;
+        g = m;
+        b = M;
+    }
+    else if (h >= 300 && h < 360) {
+        r = M;
+        g = m;
+        b = z + m;
+    }
   ////////////////////////////////////////////////////////////////////////////
 }
--- computer-graphics-raster-images/src/rgb_to_hsv.cpp
+++ computer-graphics-raster-images/src/rgb_to_hsv.cpp
@@ -1,4 +1,6 @@
 #include "rgb_to_hsv.h"
+#include <cmath>
+#include <algorithm>
 
 void rgb_to_hsv(
   const double r,
@@ -9,9 +11,43 @@ void rgb_to_hsv(
   double & v)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  h = 0;
-  s = 0;
-  v = 0;
+  double M = std::max({ r, g, b }); // get maximum rgb value
+  double m = std::min({ r, g, b }); // get minimum rgb value
+  double C = M - m; // calculate the range
+
+  v = M; // value
+
+  // saturation
+  if (v == 0) {
+	  s = 0;
+  }
+  else {
+	  s = C / v;
+  }
+
+  //hue
+  if (M == r) {
+	  h = 60 * std::fmod((g - b) / C, 6);
+  }
+  else if (M == g) {
+	  h = 60 * ((b - r) / C) + 2;
+  }
+  else if (M == b) {
+	  h = 60 * ((r - g) / C) + 4;
+  }
+  else {
+	  h = 0; // hue is undefined
+  }
+
+  // hue fitting into 360 degrees
+  if (h > 0) {
+	  h = std::fmod(h, 360);
+  }
+  else {
+	  h = h * (-1); 
+	  h = std::fmod(h, 360);
+	  h = 360 - h;
+  }
+
   ////////////////////////////////////////////////////////////////////////////
 }
