f1ae6831e612fe8451b938ad36db700d992c9cbb
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-24 18:49:45 -0400

    Add write_ppm function and eigen folder

--- computer-graphics-ray-casting/src/write_ppm.cpp
+++ computer-graphics-ray-casting/src/write_ppm.cpp
@@ -11,7 +11,56 @@ bool write_ppm(
   const int num_channels)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code from computer-graphics-raster-images
-  return false;
+  /* Reference Sources used to write this function:
+        - https://en.wikipedia.org/wiki/Netpbm#File_format_description
+        - https://cplusplus.com/forum/general/208835/
+        - https://stackoverflow.com/questions/28896001/read-write-to-ppm-image-file-c
+  */
+
+  // open ppm file
+    std::ofstream ppm_file(filename);
+
+    if (ppm_file.is_open()) {
+        // defin magic number based on number of channels
+        std::string magic_number = "P2";
+        if (num_channels == 3) {
+            magic_number = "P3";
+        }
+
+        // add header : magic number, width, height, max intensity (255 for rgb || 1 for grayscale)
+        // ppm_file << "P3 " + std::to_string(width) + " " + std::to_string(height) + " 255" << std::endl;
+        ppm_file << magic_number << std::endl;
+        ppm_file << width << " " << height << std::endl;
+        ppm_file << 255 << std::endl;
+
+        // add data
+        int img_size = width * height * num_channels;
+        for (int i = 0; i < img_size; ++i) {
+
+            ppm_file << (unsigned int)data[i];
+            // if it's a grayscale img repeate the same value 3 times 
+            //if (num_channels == 1) {
+            //    ppm_file << (unsigned int) data[i] << " " << (unsigned int)data[i];
+            //}
+            // check the approptiate spacing 
+            if ((i + 1) % (width * num_channels) == 0) {
+                ppm_file << std::endl; // new line at the end of each row
+            }
+            else if ((i + 1) % num_channels == 0) {
+                ppm_file << "   "; // add double spacing every pixel value (= channel value *3)
+            }
+            else {
+                ppm_file << " "; // add single space between channel values
+            }
+        }
+
+        // close file
+        ppm_file.close();
+        return true;
+    }
+    else {
+        std::cout << "Error: could not open file" << filename << std::endl;
+    }
+    return false;
   ////////////////////////////////////////////////////////////////////////////
 }
