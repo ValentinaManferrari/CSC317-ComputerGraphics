db5e2c9dddc1d27476cb51dca9704bf52763a8d0
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-25 17:15:47 -0400

    viewing_ray function completed

--- computer-graphics-ray-casting/src/viewing_ray.cpp
+++ computer-graphics-ray-casting/src/viewing_ray.cpp
@@ -9,7 +9,23 @@ void viewing_ray(
   Ray & ray)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+    /* Reference Sources used to write this function:
+        - Texbook Section 4.3.2: Perspective Views
+    */
+
+    // Compute u and v using textboom formula (4.1)
+    // u = l + (r - l)(i + 0.5) / n_x;
+    double u = -camera.width / 2 + camera.width * (i + 0.5) / width;
+
+    // v = b + (t - b)(j + 0.5) / n_y;
+    double v = -camera.height / 2 + camera.height * (j + 0.5) / height;
+
+    // Ray Direction <-- uU + vV -dW 
+    ray.direction = Eigen::Vector3d(u * camera.u + v * camera.v - camera.d * camera.w );
+    
+    // Ray Origin <-- e
+    ray.origin = camera.e;
+
   ////////////////////////////////////////////////////////////////////////////
 }
 
