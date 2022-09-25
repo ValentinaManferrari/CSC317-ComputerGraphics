0a67d341a9693be191460d8f8d03fb5a8c081ee4
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-25 19:56:05 -0400

    fixed bug in viewing_ray

--- computer-graphics-ray-casting/src/viewing_ray.cpp
+++ computer-graphics-ray-casting/src/viewing_ray.cpp
@@ -14,11 +14,11 @@ void viewing_ray(
     */
 
     // Compute u and v using textboom formula (4.1)
-    // u = l + (r - l)(i + 0.5) / n_x;
-    double u = -camera.width / 2 + camera.width * (i + 0.5) / width;
+    // u = l + (r - l)(j + 0.5) / n_x;
+    double u = -camera.width / 2 + camera.width * (j + 0.5) / width;
 
-    // v = b + (t - b)(j + 0.5) / n_y;
-    double v = -camera.height / 2 + camera.height * (j + 0.5) / height;
+    // v = b + (t - b)(i + 0.5) / n_y;
+    double v = -camera.height / 2 + camera.height * (i + 0.5) / height;
 
     // Ray Direction <-- uU + vV -dW 
     ray.direction = Eigen::Vector3d(u * camera.u + v * camera.v - camera.d * camera.w );
