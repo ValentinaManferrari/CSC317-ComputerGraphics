0c85d18fc744534800563bbb28510214d272916a
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-25 18:34:09 -0400

    first_hit function completed

--- computer-graphics-ray-casting/include/first_hit.h
+++ computer-graphics-ray-casting/include/first_hit.h
@@ -10,15 +10,13 @@
 // Find the first (visible) hit given a ray and a collection of scene objects
 //
 // Inputs:
-//   ray  ray along which to search
-//   min_t  minimum t value to consider (for viewing rays, this is typically at
-//     least the _parametric_ distance of the image plane to the camera)
-//   objects  list of objects (shapes) in the scene
+//   ray		ray along which to search
+//   min_t		minimum t value to consider (for viewing rays, this is typically at least the _parametric_ distance of the image plane to the camera)
+//   objects	list of objects (shapes) in the scene
 // Outputs:
-//   hit_id  index into objects of object with first hit
-//   t  _parametric_ distance along ray so that ray.origin+t*ray.direction is
-//     the hit location
-//   n  surface normal at hit location
+//   hit_id		index into objects of object with first hit
+//   t			_parametric_ distance along ray so that ray.origin+t*ray.direction in the hit location
+//   n			surface normal at hit location
 // Returns true iff a hit was found
 bool first_hit(
   const Ray & ray, 
