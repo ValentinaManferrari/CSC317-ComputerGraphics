ce0ece65cf07bbf95e08f5153f10e5d19aa5ea74
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-10-02 17:51:11 -0400

    DirectionalLight completed

--- computer-graphics-ray-tracing/src/DirectionalLight.cpp
+++ computer-graphics-ray-tracing/src/DirectionalLight.cpp
@@ -5,7 +5,9 @@ void DirectionalLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+	d = -(this->d).normalized(); // direction from point toward light as a vector.
+	max_t = numeric_limits<double>::infinity(); //parametric distance from q along d to light (by definition of Directional Light will be inf)
+	return;
   ////////////////////////////////////////////////////////////////////////////
 }
 
