9e4cf6d0608196e336e34345b8f4354cc5494755
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-10-02 17:47:50 -0400

    PointLight completed

--- computer-graphics-ray-tracing/src/PointLight.cpp
+++ computer-graphics-ray-tracing/src/PointLight.cpp
@@ -4,6 +4,8 @@ void PointLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
+	d = (this->p - q).normalized(); // direction from point toward light as a vector
+	max_t = (this->p - q).norm(); // parametric distance from q along d to light (may be inf)
+	return;
   ////////////////////////////////////////////////////////////////////////////
 }
