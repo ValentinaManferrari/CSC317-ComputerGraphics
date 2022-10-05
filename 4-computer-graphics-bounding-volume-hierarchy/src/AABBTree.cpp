7730973609550f5c2b3e4aed8333499846e4c5f1
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-10-05 15:36:57 -0400

    Cloned Lab4 repo

---
+++ 4-computer-graphics-bounding-volume-hierarchy/src/AABBTree.cpp
@@ -0,0 +1,13 @@
+#include "AABBTree.h"
+#include "insert_box_into_box.h"
+
+AABBTree::AABBTree(
+  const std::vector<std::shared_ptr<Object> > & objects,
+  int a_depth): 
+  depth(std::move(a_depth)), 
+  num_leaves(objects.size())
+{
+  ////////////////////////////////////////////////////////////////////////////
+  // Add your code here
+  ////////////////////////////////////////////////////////////////////////////
+}
