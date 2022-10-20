57e277ebcac7e7fe8109207860322681b8683b91
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-10-20 16:18:54 -0400

    insert box into box draft completed

--- 4-computer-graphics-bounding-volume-hierarchy/src/insert_box_into_box.cpp
+++ 4-computer-graphics-bounding-volume-hierarchy/src/insert_box_into_box.cpp
@@ -5,7 +5,11 @@ void insert_box_into_box(
   BoundingBox & B)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+    for (int axis = 0; axis < 3; axis++) {
+        B.min_corner[axis] = std::min(A.min_corner[axis], B.min_corner[axis]);
+        B.max_corner[axis] = std::max(A.max_corner[axis], B.max_corner[axis]);
+    }
+    return;
   ////////////////////////////////////////////////////////////////////////////
 }
 
