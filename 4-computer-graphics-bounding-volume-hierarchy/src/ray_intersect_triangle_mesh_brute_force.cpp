1bfc2211c64b23472a68a9693027dfc6f3e436fb
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-10-20 12:10:23 -0400

    ray intersect with triangle soup draft completed

--- 4-computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle_mesh_brute_force.cpp
+++ 4-computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle_mesh_brute_force.cpp
@@ -11,9 +11,23 @@ bool ray_intersect_triangle_mesh_brute_force(
   int & hit_f)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  hit_t = 0;
-  hit_f = 0;
-  return false;
+    bool hit_found = false;
+    hit_t = std::numeric_limits<double>::infinity();  
+    double new_hit_t = 0.0; 
+    // loop through all triangles in mesh
+    for (int i = 0; i < F.rows(); i++) {
+        // get triangle vertices
+        Eigen::RowVector3d A = V.row(F(i, 0));
+        Eigen::RowVector3d B = V.row(F(i, 1));
+        Eigen::RowVector3d C = V.row(F(i, 2));
+        // check for intersection
+        bool intersection = ray_intersect_triangle(ray, A, B, C, min_t, max_t, new_hit_t);
+        if (intersection && new_hit_t < hit_t) {
+            hit_t = new_hit_t;
+            hit_f = i;
+            hit_found = true;
+        }
+    }
+    return hit_found;
   ////////////////////////////////////////////////////////////////////////////
 }
